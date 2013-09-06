bc7c2437a48bafbfb8cc9e15bc8c2cb55aade737
FlackAP <FlackAP@Gmail.com>
2013-09-06 15:00:28 -0400

    further cleanup of scripts for sustainability

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,181 +1,106 @@
+// the array of stuff for our menu
+var menu = [
+  {
+    type: 'meat',
+    name: 'Salmon',
+    price: 12,
+    id: 1
+  },
+  {
+    type: 'meat',
+    name: 'Steak',
+    price: 10,
+    id: 2
+  },
+  {
+    type: 'meat',
+    name: 'Chicken',
+    price: 9,
+    id: 3
+  },
+  {
+  	type: "side",
+  	name: "Spinach",
+  	price: 4,
+  	id: 4
+  },
+  {
+  	type: "side",
+  	name: "Mac n Cheese",
+  	price: 3,
+  	id: 5
+  },
+  {
+  	type: "side",
+  	name: "Potatoes",
+  	price: 3,
+  	id: 6
+  },
+  {
+  	type: "bev",
+  	name: "Beer",
+  	price: 4,
+  	id: 7
+  },
+  {
+  	type: "bev",
+  	name: "Soda",
+  	price: 2,
+  	id: 8
+  },
+  {
+  	type: "bev",
+  	name: "Milk",
+  	price: 3,
+  	id: 9
+  }
+]
+ 
+// the array of things that have been ordered
+order = []
+ 
+// renders buttons and adds a click event to each
+// `list` - expects an array of items,
+// `foodType` - a type 'string' for filtering only one type of items
+// `parentElem` - a jQuery object in which to add the rendered buttons
+ 
+function renderList(list, foodType, parentElem) {
+ 
+  // get an array of only one type of food (based on type argument)
+  var filteredList = _.where(list, {type: foodType})
+ 
+  // loop through array of filtered foods
+  filteredList.forEach(function(item){
+ 
+    // the template for creating a button
+    var elem = $('<div class="btn btn-succes' + foodType + '" id="'+ item.id +'">' + item.name +'</div>')
+    
+    // click event for this button
+    elem.click(function(){
+      addToOrder(this)
+    })
+ 
+    // add the button to its parent element
+    parentElem.append(elem)
+  })
+ 
+}
+ 
+// example of an addToOrder thing
+function addToOrder(item) {
+ 
+  itemObject = _.findWhere(coolFoods, {id: $(item).attr(id) })
+ 
+  order.push(itemObject)
+ 
+  adjustPrice()
+ 
+}
+ 
+//  on document ready
 $(document).ready(function(){
-
-var meat = [
-	{
-		name: "Salmon",
-		price: 12,
-		id: 1
-	};
-	{
-		name: "Steak",
-		price: 11,
-		id: 2
-	}
-	{
-		name: "Chicken",
-		price: 9,
-		id: 3
-	}
-];
-
-var side = [
-	{
-		name: "Spinach",
-		price: 4.50,
-		id: 1
-	}
-	{
-		name: "Mac n Cheese",
-		price: 5,
-		id: 2
-	}
-		{
-		name: "Potatoes",
-		price: 3,
-		id: 3
-	}
-]
-
-var bev = [
-	{
-		name: "Beer",
-		price: 4.50,
-		id: 1
-	}
-	{
-		name: "Soda",
-		price: 2.50,
-		id: 2
-	}
-	{
-		name: "Milk",
-		price: 3.50,
-		id: 3
-	}
-]
-
-var meatButton = $('<div class="btn btn-primary btn-lg ourbutton" id='+ meat.id +'>'+ meat.name + '</div>')
-
-var sideButton = $('<div class="btn btn-primary btn-lg ourbutton" id='+ side.id +'>'+ side.name + '</div>')
-
-var bevButton = $('<div class="btn btn-primary btn-lg ourbutton" id='+ bev.id +'>'+ bev.name + '</div>')
-
-});
+  
+  renderList(menu, 'meat', $('.meat-container'))
+ 
+})
 	
-// 	$('.button1').click(function (list) {
-// 		var salmon = new Inventory
-// 		salmon.order ([12])
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Salmon<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$12</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-// 	$('.button2').click(function (list) {
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Steak<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$11</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-// 	$('.button3').click(function (list) {
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Chicken<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$11</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-// 	$('.button4').click(function (list) {
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Spinach<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$11</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-
-// 	$('.button5').click(function (list) {
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Macaroni & Cheese<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$11</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-
-// 	$('.button6').click(function (list) {
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Baked Potatoe<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$11</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-
-// 	$('.button7').click(function (list) {
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Beer<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$4</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-// 	$('.button8').click(function (list) {
-
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Coffee<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$2</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-// 	$('.button9').click(function (list) {
-
-// 		var ul = $('.food-list')
-// 		var ul2 = $('.price-list')
-//  		list.forEach(function(user){
-//  			var menuItem = '<li>Milk<li>';
-//  			ul.append(menuItem);
-//  			var menuPrice = '<li>$3.50</li>';
-//  			ul2.append(menuPrice);
-//  		})
-// 	});
-
-// Ticket= function (items) {
-// 	this.items= items || [];
-
-// 	this.order = function (item) {  //will push items into array
-// 		this.items.push(item)
-//   	};
-// };
-
-
-// })
-
